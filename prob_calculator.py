ab67d39cdb395400066e25461b6c8d80049855f2
Tezcatlipoca <tezcatlipoca0000@gmail.com>
2022-11-20 10:01:01 -0600

    My solution to FCC challenge

--- prob_calculator.py
+++ prob_calculator.py
@@ -35,51 +35,15 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         sample_obj = dict()
         for item in sample :
             sample_obj[item] = sample_obj.get(item, 0) + 1
-        print('the test', sample_obj, expected_balls)
-        '''
-        check = all(item in expected for item in sample)
-        if check :
-            ocurrances = ocurrances + 1
-            print('the test', sample, expected)
-        '''
-    
-    probability = ocurrances / num_experiments
-    return probability
-
-
-    ''' 
-    # I liked the random.sample() method.... too bad it doesn't comply with the FCC tests
-
-    class Hat:
-    contents = []
-
-    def __init__(self, **args) :
-        self.contents = []
-        for k,v in args.items() :
-            for i in range(v) :
-                self.contents.append(k)
-
-    def draw(self, draw) :
-        if draw > len(self.contents) :
-            draw = len(self.contents)
-        sample = random.sample(self.contents, draw)
-        return sample
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    expected = []
-    ocurrances = 0
-
-    for k,v in expected_balls.items() :
-        for i in range(v) :
-            expected.append(k)
-
-    for i in range(num_experiments) :
-        sample = hat.draw(num_balls_drawn) 
-        check = all(item in sample for item in expected)
-        if check :
+        ocurred = True
+        for k,v in expected_balls.items() :
+            try :
+                if sample_obj[k] < expected_balls[k] :
+                    ocurred = False
+            except :
+                ocurred = False
+        if ocurred :
             ocurrances = ocurrances + 1
     
     probability = ocurrances / num_experiments
     return probability
-
-'''
